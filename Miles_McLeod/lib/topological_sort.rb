52cffc3100349d75689eac997a5d1ef571b0e442
Miles McLeod <milesmcleod123@gmail.com>
2018-01-09 11:07:09 -0800

    Complete topo sort

--- Miles_McLeod/lib/topological_sort.rb
+++ Miles_McLeod/lib/topological_sort.rb
@@ -1,7 +1,22 @@
 require_relative 'graph'
+require 'byebug'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
-
+  sorted_list = []
+  queue = []
+  vertices.each { |vtx| queue.push(vtx) if vtx.in_edges.empty? }
+  until queue.empty?
+    current = queue.shift
+    sorted_list.push(current)
+    current.out_edges.each do |edge|
+      if edge.to_vertex.in_edges.length == 1
+        queue.push(edge.to_vertex)
+      end
+    end
+    current.out_edges.each { |edge| edge.destroy! }
+  end
+  return [] if sorted_list.length < vertices.length
+  sorted_list
 end
