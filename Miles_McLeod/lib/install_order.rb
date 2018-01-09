5e8587513542957b3efe0765b1886ee3ea2bd6eb
Miles McLeod <milesmcleod123@gmail.com>
2018-01-09 11:36:31 -0800

    Complete install_order problem

--- Miles_McLeod/lib/install_order.rb
+++ Miles_McLeod/lib/install_order.rb
@@ -7,8 +7,30 @@
 
 # Import any files you need to
 
-
+require 'topological_sort'
+require 'byebug'
 
 def install_order(arr)
-
+  vertices = []
+  max_id = 0
+  arr.each do |t|
+    if t[0] > max_id
+      max_id = t[0]
+    end
+    if t[1] > max_id
+      max_id = t[1]
+    end
+  end
+  max_id.times do |i|
+    vertices.push(Vertex.new(i + 1))
+  end
+  arr.each do |t|
+    dependency = vertices.select { |vtx| vtx.value == t[1] }
+    dependency = dependency[0]
+    package = vertices.select { |vtx| vtx.value == t[0] }
+    package = package[0]
+    Edge.new(dependency, package)
+  end
+  # debugger
+  topological_sort(vertices).map { |v| v.value }
 end
